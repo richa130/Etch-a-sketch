dce78ae743a795a1ec16b8a6ad1f4a6eab067b37
richa130 <richabavadekar@me.com>
2022-07-14 13:17:32 -0700

    Create initial UI w/ css/html

---
+++ sketch.js
@@ -0,0 +1,7 @@
+/* next step: figure out how to add the individual grid/divs in JS. we definitely need a nexted for loop (now up to 16 but eventually 
+user input based on the slider). we want them to appear as a grid, instead of one one each line. we could try using inline block. 
+we want borders to visually differentiate them but we dont want them to affect the size. how do we make them fill up the size of the
+div we have already created (make height height of div/16 and width width of div/16?) but we want them to be squares meaning we 
+either change the div to a sqare or pick one side to be the user input and just fill up the rest of the screen with squares according
+to that side
+*/
