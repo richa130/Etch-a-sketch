0d1978d4476ba2cda1bc402ae7cfd1211a03768c
richa130 <richabavadekar@me.com>
2022-07-15 12:54:11 -0700

    Add range slider functionality

--- README.md
+++ README.md
@@ -1,8 +1,9 @@
-This project is an Etch-a-sketch/sketchpad recreation. I am hoping that it will really solidify my understanding of DOM manipulation with JavaScript. 
+This project is an Etch-a-sketch/sketchpad recreation. I am hoping that it will solidify my understanding of DOM manipulation with JavaScript. 
 
 The process: 
 
-I started off by making the UI using HTML and CSS. I used a flexbox for the left (title/sketchpad) and right (buttons/options) sides. 
-I made flex-grow 3 for the left and 1 for the right, and I thought the proportion/overall look was good.  
+I started off by making the UI using mainly HTML and CSS, but also a bit of JavaScript to create the grid of divs using a nested for loop.
 
-Then, I used JavaScript to create the grid of square divs. This was pretty challenging to figure out. I'm still not super familiar with DOM manipulation, so there were many small issues that kept on happening. For one, 
+I created two functions: drawGrid, which takes a gridSize as parameter and draws the corresponding grid, and removeGrid, which gets a nodelist of all the div grids, iterates through the list, and removes each div.
+
+To create the range slider functionality, I used the oninput event and called both drawGrid (with the new input as a parameter), and removeGrid. The oninput event occurs anytime the input is changed at all (so even while the slider is moving), while the onchange event occurs only when the input changes from one value to another. I used oninput because I think it creates a more dynamic look as you can see the grid moving when you change the slider.
--- sketch.js
+++ sketch.js
@@ -1,24 +1,42 @@
+"use strict";
+
 const sketchpad = document.querySelector('.sketchpad');
 
 // using slice() to get rid of the last two chars and the unary plus to convert from string to number (eg. 560px -> 560)
 const sketchpadHeight = +(sketchpad.style.height.slice(0, -2));
 const sketchpadWidth = +(sketchpad.style.width.slice(0, -2));
-console.log(`sketchpadHeight: ${sketchpadHeight}, sketchpadWidth: ${sketchpadWidth}`);
 
-let gridSize = 15;
-let divSide = sketchpadHeight / gridSize;
+let slider = document.querySelector('.slider');
+let sliderValue = slider.value;
 
-// nested for loop to create the grid of divs
-for(let i = 0; i < gridSize; i++) {
-    for(let j = 0; j < gridSize; j++) {
-        let currDiv = document.createElement('div');
+// draws initial grid (sliderValue is set to a default of 15)
+drawGrid(sliderValue);
 
-        // made sure to convert num back to string and add the 'px' chars, subtracted 1 for the border t,b,l,r
-        currDiv.style.width = `${divSide-1}px`;
-        currDiv.style.height = `${divSide-1}px`;
-    
-        currDiv.style.border = '0.5px solid black';
-    
-        sketchpad.appendChild(currDiv);
+slider.oninput = function() {
+    removeGrid();
+    drawGrid(this.value);
+}
+
+function drawGrid(gridSize) {
+    let divSide = sketchpadHeight / gridSize;
+
+    // nested for loop to create the grid of divs
+    for(let i = 0; i < gridSize; i++) {
+        for(let j = 0; j < gridSize; j++) {
+            let currDiv = document.createElement('div');
+
+            // made sure to convert num back to string and add the 'px' chars, subtracted 1 for the border t,b,l,r
+            currDiv.style.width = `${divSide-1}px`;
+            currDiv.style.height = `${divSide-1}px`;
+        
+            currDiv.style.border = '0.5px solid black';
+        
+            sketchpad.appendChild(currDiv);
+        }
     }
+}
+
+function removeGrid() {
+    let sketchpadGrid = document.querySelectorAll('.sketchpad div');
+    sketchpadGrid.forEach(element => sketchpad.removeChild(element));
 }
