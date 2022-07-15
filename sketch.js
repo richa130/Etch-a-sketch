84eac5fbc45569f3ed16300aa383822af3f104b3
richa130 <richabavadekar@me.com>
2022-07-14 22:46:56 -0700

    Add started div grid UI

--- sketch.js
+++ sketch.js
@@ -1,7 +1,31 @@
-/* next step: figure out how to add the individual grid/divs in JS. we definitely need a nexted for loop (now up to 16 but eventually 
-user input based on the slider). we want them to appear as a grid, instead of one one each line. we could try using inline block. 
-we want borders to visually differentiate them but we dont want them to affect the size. how do we make them fill up the size of the
-div we have already created (make height height of div/16 and width width of div/16?) but we want them to be squares meaning we 
-either change the div to a sqare or pick one side to be the user input and just fill up the rest of the screen with squares according
-to that side
-*/
+const sketchpad = document.querySelector('.sketchpad');
+
+// using slice() to get rid of the last two chars and the unary plus to convert from string to number (eg. 560px -> 560)
+const sketchpadHeight = +(sketchpad.style.height.slice(0, -2));
+const sketchpadWidth = +(sketchpad.style.width.slice(0, -2));
+
+let divSide = sketchpadHeight / 30;
+console.log(divSide);
+let gridWidth = Math.floor(sketchpadWidth / divSide);
+console.log(gridWidth);
+
+// nested for loop to create the grid of divs
+for(let i = 0; i < 30; i++) {
+    for(let j = 0; j < gridWidth; j++) {
+        let currDiv = document.createElement('div');
+    
+        // inline-block allows the divs to appear one after the other, and allows editing of height/width
+        currDiv.style.display = 'inline-block';
+    
+        // made sure to convert num back to string and add the 'px' chars; -2 for the border t,b,l,r
+        currDiv.style.width = `${divSide-2}px`;
+        currDiv.style.height = `${divSide-2}px`;
+    
+        // makes the inline-block rows not have a gap
+        currDiv.style.marginBottom = '-7px'; 
+    
+        currDiv.style.border = '1px solid black';
+    
+        sketchpad.appendChild(currDiv);
+    }
+}
