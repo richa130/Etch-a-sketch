7eed5581f7377cafe8d58bc759da67f94705d5d3
richa130 <richabavadekar@me.com>
2022-07-15 11:56:27 -0700

    Complete UI

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-YAY :D
+This project is an Etch-a-sketch/sketchpad recreation. I am hoping that it will really solidify my understanding of DOM manipulation with JavaScript. 
+
+The process: 
+
+I started off by making the UI using HTML and CSS. I used a flexbox for the left (title/sketchpad) and right (buttons/options) sides. 
+I made flex-grow 3 for the left and 1 for the right, and I thought the proportion/overall look was good.  
+
+Then, I used JavaScript to create the grid of square divs. This was pretty challenging to figure out. I'm still not super familiar with DOM manipulation, so there were many small issues that kept on happening. For one, 
--- sketch.js
+++ sketch.js
@@ -3,28 +3,21 @@ const sketchpad = document.querySelector('.sketchpad');
 // using slice() to get rid of the last two chars and the unary plus to convert from string to number (eg. 560px -> 560)
 const sketchpadHeight = +(sketchpad.style.height.slice(0, -2));
 const sketchpadWidth = +(sketchpad.style.width.slice(0, -2));
+console.log(`sketchpadHeight: ${sketchpadHeight}, sketchpadWidth: ${sketchpadWidth}`);
 
-let divSide = sketchpadHeight / 30;
-console.log(divSide);
-let gridWidth = Math.floor(sketchpadWidth / divSide);
-console.log(gridWidth);
+let gridSize = 15;
+let divSide = sketchpadHeight / gridSize;
 
 // nested for loop to create the grid of divs
-for(let i = 0; i < 30; i++) {
-    for(let j = 0; j < gridWidth; j++) {
+for(let i = 0; i < gridSize; i++) {
+    for(let j = 0; j < gridSize; j++) {
         let currDiv = document.createElement('div');
+
+        // made sure to convert num back to string and add the 'px' chars, subtracted 1 for the border t,b,l,r
+        currDiv.style.width = `${divSide-1}px`;
+        currDiv.style.height = `${divSide-1}px`;
     
-        // inline-block allows the divs to appear one after the other, and allows editing of height/width
-        currDiv.style.display = 'inline-block';
-    
-        // made sure to convert num back to string and add the 'px' chars; -2 for the border t,b,l,r
-        currDiv.style.width = `${divSide-2}px`;
-        currDiv.style.height = `${divSide-2}px`;
-    
-        // makes the inline-block rows not have a gap
-        currDiv.style.marginBottom = '-7px'; 
-    
-        currDiv.style.border = '1px solid black';
+        currDiv.style.border = '0.5px solid black';
     
         sketchpad.appendChild(currDiv);
     }
